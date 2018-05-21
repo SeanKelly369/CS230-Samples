79bce8978e33fe73527fbf66c709a5ac18ef23d1
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-05-21 20:55:00 +0100

    Update XHR_AJAX_Object.js

--- XHR_AJAX_Object.js
+++ XHR_AJAX_Object.js
@@ -1,14 +1,17 @@
 document.getElementById('button').addEventListener('click', loadData);
 
+//Load , make new, open it, get it, load it, use it, send it
+
 function loadData() {
   // Create an XHR Object
   const xhr = new XMLHttpRequest();
 
-  // OPEN
+  // OPEN.  true, in that the code is asynchronous
   xhr.open('GET', 'data.txt', true);
 
   xhr.onload = function() {
     if(this.status === 200) {
+      //use something with the data received
       console.log(this.responseText);
     }
   }
